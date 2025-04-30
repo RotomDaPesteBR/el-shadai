06530159357e8a0df6de52dcfce2c8078608ebc1
Rotom da Peste - BR <mielsen.gamer2005@gmail.com>
2025-04-30 14:33:36 -0300

    Added route protection

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,5 +1,5 @@
-import { Link } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 import styles from './Error.module.scss';
 
 export default function Error({ ...props }) {
